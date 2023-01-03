66ecd848869ee345686aee2ecdf7b83fa8e87961
fzhu18 <fongching7@gmail.com>
2023-01-02 21:02:46 -0800

    1blog

---
+++ new post.R
@@ -0,0 +1,8 @@
+source("~/Documents/GitHub/Byronchu_Club/Create_new_post.R")
+Create_new_post(
+   "How to install pythhon and jupyter", 
+  description = "Before beginner's guide",
+  category = "R", "Python",
+  draft = FALSE
+)
+1
